df761916b9f5c110d22a78c54984df9e3d996932
jennitormala <tormalajenni@gmail.com>
2017-10-12 15:27:26 +0300

    Finish assignment

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,82 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+		int result = calculate(romanNum);
+		return result;
 	}
+
+	private int getInteger(char romanNum) {
+		switch (romanNum) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return 0;
+		}
+	}
+
+	private int calculate(String romanNum) {
+
+		if (romanNum.length() == 1) {
+			return getInteger(romanNum.charAt(0));
+		} else {
+			if (!checkForRepetition(romanNum)) {
+				return 0;
+			}
+			int number = 0;
+			int preceedingNum = 0;
+			for (int i = romanNum.length() - 1; i >= 0; i--) {
+				int temp = getInteger(romanNum.charAt(i));
+				if (temp < preceedingNum) {
+					number -= temp;
+				} else {
+					number += temp;
+					preceedingNum = temp;
+				}
+			}
+			return number;
+		}
+	}
+
+	private int checkForDuplicates(String num, String romanNums) {
+
+		char[] romanChars = romanNums.toCharArray();
+		int duplicateCount = 0;
+
+		for (int i = 0; i < romanNums.length(); i++) {
+			if (num.charAt(0) == romanChars[i]) {
+				duplicateCount += 1;
+			}
+
+		}
+		return duplicateCount;
+	}
+
+	private boolean checkForRepetition(String romanNum) {
+
+		if (romanNum.indexOf("IIII") > -1 || romanNum.indexOf("XXXX") > -1 
+		|| romanNum.indexOf("CCCC") > -1 || romanNum.indexOf("MMMM") > -1) {
+			return false;
+		} else if (checkForDuplicates("V", romanNum) > 1) {
+			return false;
+		} else if (checkForDuplicates("L", romanNum) > 1) {
+			return false;
+		} else if (checkForDuplicates("D", romanNum) > 1) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,71 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRomanNumerals_Ito1() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("I");
+		assertEquals(1, number);
 	}
-
+	@Test
+	public void testRomanNumerals_invalidInput_1() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("1");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_invalidInput_12() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("12");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_IVto4() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("IV");
+		assertEquals(4, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_VIto6() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("VI");
+		assertEquals(6, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_VV_duplicate() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("VV");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_V_repeated() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("VVX");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_L_repeated() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("LVL");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_X_repeated() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("XIIII");
+		assertEquals(0, number);
+	}
+	
+	@Test
+	public void testRomanNumerals_MMXIVto2014() {
+		RomanNumerals r = new RomanNumerals();
+		int number = r.convertToInteger("MMXIV");
+		assertEquals(2014, number);
+	}
 }
